9a9fa1d3132d4db5ef0ca1231455a372c9ce3be7
xavierrmondragon <xavierrmondragon@gmail.com>
2025-03-26 10:45:48 +0100

    Exercises for section 5.2

--- Work/stock.py
+++ Work/stock.py
@@ -4,6 +4,7 @@ class Stock:
     """
         An instance of a stock holding consisting of name, shares, and price.
     """
+    __slots__ = ('name', 'shares', 'price')
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -12,6 +13,8 @@ class Stock:
     def __repr__(self):
         return f'Stock({self.name}, {self.shares}, {self.price})'
 
+
+    @property
     def cost(self):
         """
             Return the price of the portfolio
