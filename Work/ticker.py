71a2b9f4c1f6c82e8295109e047063d29ac37e03
xavierrmondragon <xavierrmondragon@gmail.com>
2025-03-30 11:41:54 +0200

    Exercises for section 6.4

--- Work/ticker.py
+++ Work/ticker.py
@@ -17,11 +17,6 @@ def make_dicts(rows, headers):
     for row in rows:
         yield dict(zip(headers, row))
 
-def filter_symbols(rows, names):
-    for row in rows:
-        if row['name'] in names:
-            yield row
-
 def parse_stock_data(lines):
     rows = csv.reader(lines)
     rows = select_columns(rows, [0,1,4])
@@ -33,7 +28,7 @@ def ticker(portfile, logfile, fmt):
     portfolio = read_portfolio(portfile)
     lines = follow(logfile)
     rows = parse_stock_data(lines)
-    rows = filter_symbols(rows, portfolio)
+    rows = (row for row in rows if row['name'] in portfolio)
     formatter = create_formatter(fmt)
     formatter.headings(['Name', 'Price', 'Change'])
     for row in rows:
